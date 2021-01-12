c739a72dd0d27bd0f74732dda64af8516184d799
mskey <panziwenpeter@foxmail.com>
2021-01-12 15:10:48 +0800

    initial

---
+++ src/main.js
@@ -0,0 +1,50 @@
+// eslint-disable-next-line no-unused-vars
+import $ from 'jquery'
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import vueParticles from 'vue-particles'
+import Vcomp from './components/index'
+import Toast from './components/toast'
+import VueAxios from 'vue-axios'
+import axios from 'axios'
+import dataV from '@jiaminghi/data-view'
+import echarts from 'echarts'
+import {get,post} from "@/api/request";
+
+import 'element-ui/lib/theme-chalk/index.css';
+import '@/assets/styles/base.scss'
+import '@/assets/styles/common.scss'
+import '@/assets/iconfont/iconfont.css'
+import '@/assets/scss/style.scss'
+// 引入vue-awesome
+import Icon from 'vue-awesome/components/Icon'
+import 'vue-awesome/icons/index.js'
+
+Vue.use(vueParticles)
+Vue.use(Vcomp)
+Vue.use(VueAxios, axios)
+Vue.use(dataV)
+Vue.use(echarts)
+
+Vue.component('icon', Icon)
+
+Vue.config.productionTip = false
+Vue.prototype.$Toast = Toast
+Vue.prototype.$get = get
+Vue.prototype.$post = post
+
+
+router.beforeEach((to, from, next) => {
+	if (to.meta.title) {
+		document.title = to.meta.title;
+	}
+	next();
+})
+
+new Vue({
+  router,
+  store,
+  render: h => h(App),
+}).$mount('#app')
---
+++ src/views/datav/index.vue
@@ -0,0 +1,137 @@
+<template>
+  <div id="index">
+    <dv-full-screen-container class="bg">
+      <dv-loading v-if="loading">Loading...</dv-loading>
+      <div v-else class="host-body">
+        <div class="d-flex jc-center">
+          <dv-decoration-10 style="width:33.3%;height:5px;" />
+          <div class="d-flex jc-center">
+            <dv-decoration-8 :color="['#568aea', '#000000']" style="width:200px;height:50px;" />
+            <div class="title">
+              <span class="title-text">大数据可视化平台</span>
+              <dv-decoration-6
+                class="title-bototm"
+                :reverse="true"
+                :color="['#50e3c2', '#67a1e5']"
+                style="width:250px;height:8px;"
+              />
+            </div>
+            <dv-decoration-8
+              :reverse="true"
+              :color="['#568aea', '#000000']"
+              style="width:200px;height:50px;"
+            />
+          </div>
+          <dv-decoration-10 style="width:33.3%;height:5px;" />
+        </div>
+
+        <!-- 第二行 -->
+        <div class="d-flex jc-between px-2">
+          <div class="d-flex" style="width: 40%">
+            <div
+              class="react-right ml-4"
+              style="width: 500px; text-align: left;background-color: #0f1325;"
+            >
+              <span class="react-before"></span>
+              <span class="text fw-b">数据分析1</span>
+            </div>
+            <div class="react-right ml-3" style="background-color: #0f1325;">
+              <span class="text colorBlue fw-b">数据分析2</span>
+            </div>
+          </div>
+          <div style="width: 40%" class="d-flex">
+            <div class="react-left bg-color-blue mr-3">
+              <span class="text fw-b">数据分析3</span>
+            </div>
+            <div
+              class="react-left mr-4"
+              style="width: 500px; background-color: #0f1325; text-align: right;"
+            >
+              <span class="react-after"></span>
+              <span class="text">2020年03月16日 周一 12:00</span>
+            </div>
+          </div>
+        </div>
+
+        <div class="body-box">
+          <!-- 第三行数据 -->
+          <div class="content-box">
+            <div>
+              <dv-border-box-12>
+                <centreLeft1 />
+              </dv-border-box-12>
+            </div>
+            <div>
+              <dv-border-box-12>
+                <centreLeft2 />
+              </dv-border-box-12>
+            </div>
+            <!-- 中间 -->
+            <div>
+              <center />
+            </div>
+            <!-- 中间 -->
+            <div>
+              <centreRight2 />
+            </div>
+            <div>
+              <dv-border-box-13>
+                <centreRight1 />
+              </dv-border-box-13>
+            </div>
+          </div>
+
+          <!-- 第四行数据 -->
+          <div class="bototm-box">
+            <dv-border-box-13>
+              <bottomLeft />
+            </dv-border-box-13>
+            <dv-border-box-12>
+              <bottomRight />
+            </dv-border-box-12>
+          </div>
+        </div>
+      </div>
+    </dv-full-screen-container>
+  </div>
+</template>
+
+<script>
+import centreLeft1 from "./centreLeft1";
+import centreLeft2 from "./centreLeft2";
+import centreRight1 from "./centreRight1";
+import centreRight2 from "./centreRight2";
+import center from "./center";
+import bottomLeft from "./bottomLeft";
+import bottomRight from "./bottomRight";
+export default {
+  data() {
+    return {
+      loading: true
+    };
+  },
+  components: {
+    centreLeft1,
+    centreLeft2,
+    centreRight1,
+    centreRight2,
+    center,
+    bottomLeft,
+    bottomRight
+  },
+  mounted() {
+    this.cancelLoading();
+  },
+  methods: {
+    cancelLoading() {
+      setTimeout(() => {
+        this.loading = false;
+      }, 2000);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/scss/index";
+</style>
